db25202de8bf5dc42d191f1ae644970fdcaf8123
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-19 15:17:04 +0000

    feat: comment out remote state

--- terraform.tf
+++ terraform.tf
@@ -3,11 +3,11 @@ provider "aws" {
   version = "~> 2.0"
 }
 # depends if we can use tf cloud - if not - set up the state bucket?
-terraform {
+/*terraform {
   backend "s3" {
     encrypt = true
     bucket  = "dpg-november-tfstate-bucket"
     region  = "eu-west-2"
     key     = "jenkins.tfstate"
   }
-}
+}*/
