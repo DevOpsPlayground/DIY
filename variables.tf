f5a764b53ac9cc2716f9924638b4d456a2112103
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-20 12:01:09 +0000

    chore: removing unused variables

--- variables.tf
+++ variables.tf
@@ -1,18 +1,6 @@
 
-variable "security_group_id" {
-  default = "sg-0cfa06fed7a4649c7"
-}
-
-variable "subnet_id" {
-  default = "subnet-02db31a87afc4dc31"
-}
-
 variable "region" {
   default = "eu-west-2"
-}
-
-variable "UNIQUE_ANIMAL_IDENTIFIER" {
-  default = "changeme"
 }
 variable "instance_count" {
   default = 1
