6a1f759fac7ccfb437bb0fabf13f02ce662b7dd2
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-20 11:46:07 +0000

    chore: adding a count to all the modules

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 module "network" {
+  count = 1
   source         = "./modules/network"
   PlaygroundName = var.PlaygroundName
 }
 module "Jenkins_role" {
+  count = 1
   source             = "./modules/rolePolicy"
   PlaygroundName     = var.PlaygroundName
   role_policy = file("policies/assume_role.json")
@@ -13,15 +15,15 @@ module "jenkins" {
   source             = "./modules/instance"
   depends_on         = [module.network]
   PlaygroundName     = "${var.PlaygroundName}Jenkins"
-  security_group_ids = [module.network.allow_all_security_group_id]
-  subnet_id          = module.network.public_subnets.1
+  security_group_ids = [module.network.0.allow_all_security_group_id]
+  subnet_id          = module.network.0.public_subnets.0
   user_data          = file("scripts/install-jenkins.sh")
-  InstanceRole       = module.Jenkins_role.role
+  InstanceRole       = module.Jenkins_role.0.role
 }
 module "workstation" {
   count              = 1
   source             = "./modules/instance"
   PlaygroundName     = "${var.PlaygroundName}workstation"
-  security_group_ids = [module.network.allow_all_security_group_id]
-  subnet_id          = module.network.public_subnets.1
+  security_group_ids = [module.network.0.allow_all_security_group_id]
+  subnet_id          = module.network.0.public_subnets.0
 }
