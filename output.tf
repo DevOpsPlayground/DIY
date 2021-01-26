68180319aa73e0d5e5b265f46f50b42da34e68a2
James Sandford <james.sandford@ecs-digital.co.uk>
2021-01-26 13:57:01 +0000

    fix: more descriptions

--- output.tf
+++ output.tf
@@ -1,11 +1,14 @@
 output "jenkins_ips" {
   value = module.jenkins.*.public_ips
+  description = "The ip of the jenkins instances"
 }
 output "workstation_ips" {
   value = module.workstation.*.public_ips
+  description = "The ip of the workstation instances"
 }
 output "WorkstationPassword" {
   value     = var.WorkstationPassword
+  description = "The password of the workstation"
   sensitive = true
 }
 output "TFstateBucket" {
